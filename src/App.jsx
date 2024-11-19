31a6aca7084d081e85202a3dcb9c0222751f04fe
rudrayadav <rudracom429@gmail.com>
2024-11-19 12:33:53 +0530

    header create

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className="text-2xl text-center mt-11 text-red-500">We are creating Tour And Travelling Website with Ankit</div>
+    </>
+  )
+}
+
+export default App;
